0a14f60a540a6ccf033d7d7ef09121365bafc5fc
Shantam_Shukla <shantamshuklagkp@gmail.com>
2024-10-09 20:48:33 +0530

    commit2

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center p-24 gap-2">
-      User's Landing Page
+      User&apos;s Landing Page
 
       <Link className={buttonVariants({ variant: "outline" })} href={"pinecone"}>GO UPDATE!</Link>
     </main>
